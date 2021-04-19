b81b15ce79e7feefe77f2fd4671dd353b9e2e647
codefaztaz <codefaztaz@gmail.com>
2021-04-19 13:48:24 +0100

    commit 1 section 3 design

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.scss'
+  ]
+})
+export class NopagefoundComponent implements OnInit {
+  year = new Date().getFullYear();
+
+}
