c4d8df3cc58926a47cbec90eaeadfae8b6ced653
codefaztaz <codefaztaz@gmail.com>
2021-04-26 19:07:50 +0100

    I added pages and auth routing modules and pages, auth modules.

--- src/app/nopagefound/nopagefound.component.ts
+++ src/app/nopagefound/nopagefound.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./nopagefound.component.scss'
   ]
 })
-export class NopagefoundComponent implements OnInit {
+export class NopagefoundComponent {
   year = new Date().getFullYear();
 
 }
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  { 
+    path: 'dashboard', 
+    component: PagesComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+
+    ]
+      
+  },
+ 
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
